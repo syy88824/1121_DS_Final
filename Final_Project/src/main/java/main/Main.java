ebce2cdc77156c237a79d4296fef811a9b1270f3
syy88824 <jasmine.shyr@gmail.com>
2023-11-07 17:18:57 +0800

    Update Main.java

--- Final_Project/src/main/java/main/Main.java
+++ Final_Project/src/main/java/main/Main.java
@@ -1,13 +1,8 @@
 package main;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-
 public class Main{
 	
-	 public static void main(String[] args) throws SQLException {
+	 public static void main(String[] args) {
 		 
 	  }
 		 
