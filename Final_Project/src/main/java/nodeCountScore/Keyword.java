e0bc6a43563846878a5699918728eca708e1b064
syy88824 <jasmine.shyr@gmail.com>
2023-12-02 18:12:41 +0800

    2

--- Final_Project/src/main/java/nodeCountScore/Keyword.java
+++ Final_Project/src/main/java/nodeCountScore/Keyword.java
@@ -1,31 +1,5 @@
-public class Keyword {
-	public String name;
-    public int count;
-    public float weight;
-    
-    public Keyword(String name, int count, float weight){
-		this.name = name;
-		this.count = count;
-		this.weight = weight;
-    }
-    
-    @Override
-    public String toString(){
-    	return "["+name+","+count+","+weight+"]";
-    }
+package nodeCountScore;
 
-    public int getCount()
-    {
-    	return count;
-    }
-    
-    public String getName()
-    {
-    	return name;
-    }
-    
-    public float getWeight()
-    {
-    	return weight;
-    }
+public class Keyword {
+
 }
