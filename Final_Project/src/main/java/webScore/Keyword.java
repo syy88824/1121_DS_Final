7910dad8efca9e1bb2f49609d6a5ac56fcba4feb
syy88824 <117265739+syy88824@users.noreply.github.com>
2023-12-02 18:18:28 +0800

    Add files via upload

--- Final_Project/src/main/java/webScore/Keyword.java
+++ Final_Project/src/main/java/webScore/Keyword.java
@@ -1,5 +1,14 @@
-package webScore;
-
 public class Keyword {
-
+	public String name;
+	public double weight;
+	
+	public Keyword(String name, double weight){
+		this.name = name;
+		this.weight = weight;
+	}
+	
+	@Override
+	public String toString(){
+		return "["+name+","+weight+"]";
+	}
 }
