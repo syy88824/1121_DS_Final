bf8ce0aee3622155b955e8fd478d47a225405685
fredlin100 <118900490+fredlin100@users.noreply.github.com>
2023-12-31 20:22:34 +0800

    subpage

--- Final_Project/src/main/java/webCrawler/GoogleQuery.java
+++ Final_Project/src/main/java/webCrawler/GoogleQuery.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.jsoup.Jsoup;
@@ -13,126 +14,95 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class GoogleQuery 
-{
-	public String searchKeyword;
-	public String url;
-	public String content;
-	
-	public GoogleQuery(String searchKeyword)
-	{
-		this.searchKeyword = searchKeyword;
-		this.url = "http://www.google.com/search?q="+searchKeyword+"&oe=utf8&num=20";
-	}
-	
-	private String fetchContent() throws IOException
-	{
-		String retVal = "";
+import webScore.WebNode;
+import webScore.WebPage;
 
-		URL u = new URL(url);
-		URLConnection conn = u.openConnection();
-		//set HTTP header
-		conn.setRequestProperty("User-agent", "Chrome/107.0.5304.107");
-		InputStream in = conn.getInputStream();
+public class GoogleQuery {
+    public String searchKeyword;
+    public String url;
+    public String content;
+    public ArrayList<WebPage> subWebPages; // Add a list to store sub-webpages
 
-		InputStreamReader inReader = new InputStreamReader(in, "utf-8");
-		BufferedReader bufReader = new BufferedReader(inReader);
-		String line = null;
+    public GoogleQuery(String searchKeyword) {
+        this.searchKeyword = searchKeyword;
+        try {
+            String encodeKeyword = java.net.URLEncoder.encode(searchKeyword, "utf-8");
+            this.url = "https://www.google.com/search?q=" + encodeKeyword + "&oe=utf8&num=20";
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
-		while((line = bufReader.readLine()) != null)
-		{
-			retVal += line;
-		}
-		return retVal;
-	}
-	
-	public HashMap<String, String> query() throws IOException
-	{
-		if(content == null)
-		{
-			content = fetchContent();
-		}
+        this.subWebPages = new ArrayList<>();
+    }
 
-		HashMap<String, String> retVal = new HashMap<String, String>();
-		
-		
-		/* 
-		 * some Jsoup source
-		 * https://jsoup.org/apidocs/org/jsoup/nodes/package-summary.html
-		 * https://www.1ju.org/jsoup/jsoup-quick-start
- 		 */
-		
-		//using Jsoup analyze html string
-		Document doc = Jsoup.parse(content);
-		
-		//select particular element(tag) which you want 
-		Elements lis = doc.select("div");
-		lis = lis.select(".kCrYT");
-		
-		for(Element li : lis)
-		{
-			try 
-			{
-				String citeUrl = li.select("a").get(0).attr("href");
-				String title = li.select("a").get(0).select(".vvjwJb").text();
-				
-				if(title.equals("")) 
-				{
-					continue;
-				}
-				
-				System.out.println("Title: "+title + " , url: " + citeUrl);
-				
-				//put title and pair into HashMap
-				retVal.put(title, citeUrl);
+    private String fetchContent() throws IOException {
+        String retVal = "";
 
-				crawlSubpages(citeUrl, retVal);
-				
-			} catch (IndexOutOfBoundsException e) 
-			{
-//				e.printStackTrace();
-			}
-		}
-		return retVal;
-	}
-	
-	//加入爬子網頁的method
-	private void crawlSubpages(String url, HashMap<String, String> retVal) {
-	    try {
-	        
-	        String realUrl = extractRealUrl(url);
+        URL u = new URL(url);
+        URLConnection conn = u.openConnection();
+        conn.setRequestProperty("User-agent", "Chrome/107.0.5304.107");
+        InputStream in = conn.getInputStream();
 
-	        Document subPageDoc = Jsoup.connect(realUrl).get();
+        InputStreamReader inReader = new InputStreamReader(in, "utf-8");
+        BufferedReader bufReader = new BufferedReader(inReader);
+        String line = null;
 
-	        
-	        String subPageTitle = subPageDoc.title();
-	        System.out.println("子網頁標題: " + subPageTitle);
+        while ((line = bufReader.readLine()) != null) {
+            retVal += line;
+        }
+        return retVal;
+    }
 
-	        
-	        Elements paragraphs = subPageDoc.select("p");
-	        StringBuilder subPageContent = new StringBuilder();
-	        for (Element paragraph : paragraphs) {
-	            subPageContent.append(paragraph.text()).append("\n");
-	        }
-	        System.out.println("子網頁內容:\n" + subPageContent);
+    // New method to fetch sub-webpage titles and URLs
+    private void fetchSubWebPages() throws IOException {
+        Document doc = Jsoup.parse(content);
+        Elements links = doc.select("a[href]");
 
-	        
-	        retVal.put("Subpage Title: " + subPageTitle, "Subpage Content:\n" + subPageContent);
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	}
-	
-	//解析 Google 搜尋结果的連結，獲取其中真實的 URL
-	private String extractRealUrl(String googleUrl) {
-	    
-	    int startIndex = googleUrl.indexOf("/url?q=") + 7;
-	    int endIndex = googleUrl.indexOf("&sa=");
-	    if (startIndex != -1 && endIndex != -1) {
-	        return googleUrl.substring(startIndex, endIndex);
-	    } else {
-	        
-	        return googleUrl;
-	    }
-	}
+        for (Element link : links) {
+            String subPageUrl = link.absUrl("href");
+
+            if (!subPageUrl.isEmpty()) {
+                String subPageTitle = link.text();
+                WebPage subWebPage = new WebPage(subPageUrl, subPageTitle);
+                subWebPages.add(subWebPage);
+            }
+        }
+    }
+
+    public HashMap<String, String> query() throws IOException {
+        if (content == null) {
+            content = fetchContent();
+            fetchSubWebPages();
+        }
+
+        HashMap<String, String> retVal = new HashMap<>();
+
+        Document doc = Jsoup.parse(content);
+        Elements lis = doc.select("div");
+        lis = lis.select(".kCrYT");
+
+        for (Element li : lis) {
+            try {
+                String citeUrl = li.select("a").get(0).attr("href").replace("/url?q=", "");
+                String title = li.select("a").get(0).select(".vvjwJb").text();
+
+                if (title.equals("")) {
+                    continue;
+                }
+
+                System.out.println("Title: " + title + " , url: " + citeUrl);
+                retVal.put(title, citeUrl);
+
+            } catch (IndexOutOfBoundsException e) {
+                // e.printStackTrace();
+            }
+        }
+
+        for (WebPage subWebPage : subWebPages) {
+            System.out.println("Sub-Page Title: " + subWebPage.name + " , Sub-Page URL: " + subWebPage.url);
+        }
+
+        return retVal;
+    }
 }
+
